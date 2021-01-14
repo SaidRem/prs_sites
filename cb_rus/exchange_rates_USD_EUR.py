faae9e263c140ed61f20231211d4145a606b8bd5
SaidRem <said.r@yandex.ru>
2021-01-14 22:31:37 +0400

    Added reg pattern for eur/use

--- cb_rus/exchange_rates_USD_EUR.py
+++ cb_rus/exchange_rates_USD_EUR.py
@@ -3,6 +3,12 @@ import re
 
 r = requests.get('https://cbr.ru')
 page = r.text
-res_eur = re.search(r'EUR.+(\d\d,\d+).+(\d\d,\d+)', page, re.DOTALL)
-print(f'Euro on tomorrow: {res_eur.group(2)} P')
+# res_eur = re.search(r'EUR.+(\d\d,\d+).+(\d\d,\d+)', page, re.DOTALL)
+# print(f'Euro on tomorrow: {res_eur.group(2)} P')
+
+res_eur_usd = re.search(r'USD.*(\d\d,\d+).+(\d\d,\d+).+(\d\d,\d+).+(\d\d,\d+)',
+                        page, re.DOTALL)
+
+print(f'USD on tomorrow: {res_eur_usd.group(2)} P\n'
+      f'EUR on tomorrow: {res_eur_usd.group(4)} P\n')
 

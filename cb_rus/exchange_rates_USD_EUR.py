cfe94a8cc73809993dfabe63e4cc98abbe8f70c0
SaidRem <said.r@yandex.ru>
2021-01-14 22:14:02 +0400

    added regular expression to retrieve euro

--- cb_rus/exchange_rates_USD_EUR.py
+++ cb_rus/exchange_rates_USD_EUR.py
@@ -1,2 +1,8 @@
 import requests
 import re
+
+r = requests.get('https://cbr.ru')
+page = r.text
+res_eur = re.search(r'EUR.+(\d\d,\d+).+(\d\d,\d+)', page, re.DOTALL)
+print(f'Euro on tomorrow: {res_eur.group(2)} P')
+

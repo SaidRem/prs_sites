e91bd09a0ca31ffda5f0e208a1db48f5ea949ddc
SaidRem <said.r@yandex.ru>
2021-01-28 07:22:49 +0400

    Added func usdeur

--- cb_rus/exrates_usd_eur.py
+++ cb_rus/exrates_usd_eur.py
@@ -1,14 +1,18 @@
 import requests
 import re
 
-r = requests.get('https://cbr.ru')
-page = r.text
-# res_eur = re.search(r'EUR.+(\d\d,\d+).+(\d\d,\d+)', page, re.DOTALL)
-# print(f'Euro on tomorrow: {res_eur.group(2)} P')
+def usdeur():
+    r = requests.get('https://cbr.ru')
+    page = r.text
+    # res_eur = re.search(r'EUR.+(\d\d,\d+).+(\d\d,\d+)', page, re.DOTALL)
+    # print(f'Euro on tomorrow: {res_eur.group(2)} P')
+    res_eur_usd = re.search(r'USD.*(\d{2},\d{4}).+(\d{2},\d{4}).+(\d{2},\d{4}).+(\d{2},\d{4})', 
+                            page, re.DOTALL)
+    return res_eur_usd
 
-res_eur_usd = re.search(r'USD.*(\d{2},\d{4}).+(\d{2},\d{4}).+(\d{2},\d{4}).+(\d{2},\d{4})',
-                        page, re.DOTALL)
 
-print(f'USD on tomorrow: {res_eur_usd.group(2)} P\n'
-      f'EUR on tomorrow: {res_eur_usd.group(4)} P\n')
+if __name__ == '__main__':
+    res_eur_usd = usdeur()
+    print(f'USD on tomorrow: {res_eur_usd.group(2)} P\n'
+          f'EUR on tomorrow: {res_eur_usd.group(4)} P\n')
 

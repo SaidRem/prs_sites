57452e0e42f78b9a78b5cec328a1032787d89eac
SaidRem <said.r@yandex.ru>
2021-02-04 17:09:26 +0400

    Added encoding in daily_exrates() for correctly convert

--- cb_rus/cbr_exrates.py
+++ cb_rus/cbr_exrates.py
@@ -1,3 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
+import re
+from pprint import pprint
 
-URL_xml = "http://www.cbr.ru/scripts/XML_daily.asp"
+URL_exrates = "http://www.cbr.ru/scripts/XML_daily.asp"
+
+def daily_exrates():
+    page = requests.get(URL_exrates)
+    # page.content
+    page.encoding = 'windows-1251'
+    return page.text
+
+if __name__ == '__main__':
+    result = daily_exrates()
+    pprint(result)
+

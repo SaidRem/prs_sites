1376f1f06e92140060a998001d2df05f612089a9
SaidRem <said.r@yandex.ru>
2021-02-08 18:38:37 +0400

    First

--- cb_rus/cbr_exrates.py
+++ cb_rus/cbr_exrates.py
@@ -7,11 +7,16 @@ URL_exrates = "http://www.cbr.ru/scripts/XML_daily.asp"
 
 def daily_exrates():
     page = requests.get(URL_exrates)
-    # page.content
     page.encoding = 'windows-1251'
     return page.text
+
+def bs4_valute(text):
+    bs4_content = BeautifulSoup(text, 'lxml')
+    return bs4_content.find_all("valute")
+
 
 if __name__ == '__main__':
     result = daily_exrates()
     pprint(result)
-
+    print(f"Type is: {type(result)}")
+    print(bs4_valute(result).prettify())

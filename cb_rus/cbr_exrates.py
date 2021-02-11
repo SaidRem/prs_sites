1cdfa81c747c6df1e10aa016c071e51a8feffa33
SaidRem <said.r@yandex.ru>
2021-02-11 18:42:52 +0400

    Added functionality to print_table

--- cb_rus/cbr_exrates.py
+++ cb_rus/cbr_exrates.py
@@ -14,12 +14,23 @@ def bs4_valutes(text):
     bs4_content = BeautifulSoup(text, 'lxml')
     return bs4_content.find_all("valute")
 
-def print_table(bs4_content):
-    pass
+def print_table():
+    valutes_list = bs4_valutes(daily_exrates())
+    print('|{: ^44s}|{: ^20s}|{: ^20s}|{: ^20s}|'.format('Currency name',
+                                                         'Charcode',
+                                                         'Nominal',
+                                                         'Value'))
+    print('-' * 109)
+    for row in sorted(valutes_list, key=lambda x: x.find('name').text):
+        print('| {: <43s}|{: ^20s}|{: ^20s}|{: ^20s}|'.format(row.find('name').text,
+                                                              row.find('charcode').text,
+                                                              row.find('nominal').text,
+                                                              row.find('value').text))
 
 
 if __name__ == '__main__':
-    result = daily_exrates()
-    pprint(result)
-    print(f"Type is: {type(result)}")
-    print(bs4_valutes(result).prettify())
+    #result = daily_exrates()
+    #pprint(result)
+    #print(f"Type is: {type(result)}")
+    #print(bs4_valutes(result).prettify())
+    print_table()

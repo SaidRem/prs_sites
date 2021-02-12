4924037ed0c9ea8e5fa8b9ed0e4cedeab57e222a
SaidRem <said.r@yandex.ru>
2021-02-12 17:41:58 +0400

    Added description in funcs

--- cb_rus/cbr_exrates.py
+++ cb_rus/cbr_exrates.py
@@ -6,17 +6,27 @@ from pprint import pprint
 URL_exrates = "http://www.cbr.ru/scripts/XML_daily.asp"
 
 def daily_exrates():
+    "Returns page from cbr.ru/scripts/XML_daily.asp"
     page = requests.get(URL_exrates)
     page.encoding = 'windows-1251'
     return page.text
 
 def bs4_valutes(text, pretty=False):
+    """
+    Finds all content between tag <valute> in xml file.
+    Returns bs4 object.
+    """
     bs4_content = BeautifulSoup(text, 'lxml')
     result = bs4_content.find_all("valute")
     if pretty: print(bs4_content.prettify())
     return result
 
 def print_table():
+    """
+    The function prints all currencies in table with columns:
+    Currency name, Charcode, Nominal, Valute.
+    (For data see bs4_valutes())
+    """
     valutes_list = bs4_valutes(daily_exrates())
     print('|{: ^44s}|{: ^20s}|{: ^20s}|{: ^20s}|'.format('Currency name',
                                                          'Charcode',

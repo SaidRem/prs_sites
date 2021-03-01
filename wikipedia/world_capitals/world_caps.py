ce839251dd4d9cde0ad8b068cac431f74b9cc5c2
SaidRem <said.r@yandex.ru>
2021-03-01 18:51:02 +0400

    In world_caps.py parsed capitals of Europe

--- wikipedia/world_capitals/world_caps.py
+++ wikipedia/world_capitals/world_caps.py
@@ -1,8 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL_AD = ''    # page address to work with
+URL_CAPITALS = 'https://ru.wikipedia.org/wiki/Список_столиц_государств'    # page address to work with
 
-def get_page(url):
-    "Returns page"
-    pass
+page = requests.get(URL_CAPITALS)
+soup = BeautifulSoup(page.text, 'lxml')
+
+world_parts = []
+
+for n in soup.body.find_all('h2')[:-4]:
+    if n.find('a'):
+        world_parts.append(n.a.get('title'))
+
+print(world_parts)
+
+
+europe = dict.fromkeys(['Europe'], {})
+all_tr = soup.body.find_all('tbody')[0].find_all('tr')[1:-4]
+for name in all_tr:
+    europe['Europe'][name.find_all('a')[-2].text] = name.find_all('a')[-1].text
+
+print(europe)
+
+# TODO 
+# Asia, Africa, America, Australia and Oceania
